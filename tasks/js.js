d3e2b35e0d28fff8eb96f27d227ce7204b933f30
Talysson <talyssonoc@gmail.com>
2015-09-29 20:51:24 -0300

    Remove tasks

--- tasks/js.js
+++ tasks/js.js
@@ -3,10 +3,8 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var eslint = require('gulp-eslint');
-var karma = require('gulp-karma');
 
 var jsGlob = 'src/js/**/*.js';
-var specGlob = 'spec/**/*.js';
 
 gulp.task('build:js', function () {
   browserify({
@@ -24,17 +22,6 @@ gulp.task('lint:js', function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
-});
-
-gulp.task('test:js', function() {
-  return gulp.src(specGlob)
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'start'
-    }))
-    .on('error', function(err) {
-      throw err;
-    });
 });
 
 gulp.task('watch:js', ['build:js'], function() {
